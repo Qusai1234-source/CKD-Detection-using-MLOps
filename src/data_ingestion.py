d547bb69f3c97769625b5386021ed2459d0b9743
Qusai1234-source <qmadrasa@gitam.in>
2025-12-17 19:00:58 +0530

    Data Ingestion module comleted

---
+++ src/data_ingestion.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+from log import get_logger
+from tensorflow.keras.image import image_dataset_from_directory
+
+get_logger("data_Ingestion")
+
+def validate_data(file_path):
+    
+def load_data()
---
+++ src/log.py
@@ -0,0 +1,24 @@
+import logging
+import os
+
+from asyncssh import logger 
+
+
+def get_logger(file_name):
+
+    log_dir="logs"
+    os.makedirs(log_dir,exist_ok=True)
+
+    logger=logging.getLogger(file_name)
+    logger.setLevel("DEBUG")
+
+    consoleHandler=logging.StreamHandler()
+    consoleHandler.setLevel("DEBUG")
+
+    log_file_path=os.path.join(log_dir,file_name +".log")
+    fileHandler=logging.FileHandler(log_file_path)
+    fileHandler.setLevel("DEBUG")
+
+    formatter=logging.Formatter("%(asctime)s -%(name)s - %(levelname)s - %(message)s")
+    consoleHandler.setFormatter(formatter)
+    fileHandler.setFormatter(formatter)
